3da6fecfd7c151fcc3f2b9e8193b1505fa43bca9
François Agrech <f.agrech@gmail.com>
2013-06-28 18:12:57 +0200

    Improve error handling in libflorence.

--- src/lib/florence.c
+++ src/lib/florence.c
@@ -28,12 +28,14 @@ struct florence {
 	guint dbus_id;
 	GDBusConnection *connection;
 	terminate_func terminate;
+	florence_error error;
 };
 struct florence *florence=NULL;
 
 /* Called when dbus name has appeared */
 void florence_on_name_appeared(GDBusConnection *con, const gchar *name, const gchar *name_owner, gpointer user_data)
 {
+	florence->error=FLORENCE_SUCCESS;
 	florence->connection=con;
 	g_main_loop_quit(florence->loop);
 }
@@ -41,13 +43,31 @@ void florence_on_name_appeared(GDBusConnection *con, const gchar *name, const gc
 /* Called when dbus name has vanished */
 void florence_on_name_vanished(GDBusConnection *con, const gchar *name, gpointer user_data)
 {
+	florence->error=FLORENCE_FAIL;
 	if (florence->terminate) florence->terminate();
+	g_main_loop_quit(florence->loop);
+}
+
+/* Exit main loop after timeout */
+gboolean florence_timeout(gpointer user_data)
+{
+	florence->error=FLORENCE_TIMEOUT;
+	g_main_loop_quit(florence->loop);
+	return FALSE;
+}
+
+/* Exit the main loop when done */
+void florence_done (GObject *source_object, GAsyncResult *res, gpointer user_data)
+{
+	florence->error=FLORENCE_SUCCESS;
 	g_main_loop_quit(florence->loop);
 }
 
 /* Initialize the dbus connection */
 florence_error florence_init(terminate_func terminate)
 {
+	guint source_id;
+	g_type_init();
 	florence=(struct florence *)g_malloc(sizeof(struct florence));
 	if (!florence) return FLORENCE_FAIL;
 	memset(florence, 0, sizeof(struct florence));
@@ -56,49 +76,39 @@ florence_error florence_init(terminate_func terminate)
 		G_BUS_NAME_WATCHER_FLAGS_NONE, florence_on_name_appeared,
 		florence_on_name_vanished, florence, NULL);
 	florence->loop = g_main_loop_new(NULL, FALSE);
-	return FLORENCE_SUCCESS;
+	/* 3 seconds timeout */
+	source_id=g_timeout_add_seconds(3, florence_timeout, florence);
+	g_main_loop_run(florence->loop);
+	g_source_remove(source_id);
+	return florence->error;
 }
 
 /* Free memory */
 florence_error florence_exit()
 {
 	if (!florence) return FLORENCE_FAIL;
-	g_main_loop_unref(florence->loop);
 	g_bus_unwatch_name(florence->dbus_id);
+	g_main_loop_unref(florence->loop);
 	g_free(florence);
 	return FLORENCE_SUCCESS;
-}
-
-/* Exit main loop after timeout */
-gboolean florence_timeout(gpointer user_data)
-{
-	g_main_loop_quit(florence->loop);
-	return FALSE;
-}
-
-/* Exit the main loop when done */
-void florence_done (GObject *source_object, GAsyncResult *res, gpointer user_data)
-{
-	g_main_loop_quit(florence->loop);
 }
 
 /* Send a dbus command to florence Keyboard object */
 florence_error florence_send(gchar *command)
 {
+	guint source_id;
 	if (florence) {
-		if (!florence->connection) {
-			/* 3 seconds timeout */
-			g_timeout_add_seconds(3, florence_timeout, florence);
-			g_main_loop_run(florence->loop);
-		}
 		if (florence->connection) {
 			g_dbus_connection_call(florence->connection, "org.florence.Keyboard",
 				"/org/florence/Keyboard", "org.florence.Keyboard", command,
 				NULL, NULL, G_DBUS_CALL_FLAGS_NONE, -1, NULL, florence_done, NULL);
+			/* 3 seconds timeout */
+			source_id=g_timeout_add_seconds(3, florence_timeout, florence);
 			g_main_loop_run(florence->loop);
-		}
+			g_source_remove(source_id);
+		} else return FLORENCE_FAIL;
 	} else return FLORENCE_FAIL;
-	return florence->connection?FLORENCE_SUCCESS:FLORENCE_TIMEOUT;
+	return florence->error;
 }
 
 /* Show the keyboard */
