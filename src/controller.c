4341941f560d144d61da8a537ab689546b5cf37d
François Agrech <f.agrech@gmail.com>
2014-05-12 20:39:56 +0200

    Fix distcheck reported problems.
    Fix docbook validation problem in documentation.
    Add missing files in POTFILES.in
    Add symbols.xml.in to distclean target.

--- src/controller.c
+++ src/controller.c
@@ -192,7 +192,7 @@ void controller_focus_event (const AtspiEvent *event, void *user_data)
 	AtspiStateSet *state_set=atspi_accessible_get_state_set(event->source);
 	AtspiRole role=atspi_accessible_get_role(event->source, &error);
 	if (error) flo_error(_("Event error: %s"), error->message);
-	flo_debug(TRACE_DEBUG, _("Receives atspi focus event. Object role=%d"), role);
+	flo_debug(TRACE_DEBUG, _("ATSPI focus event received. Object role=%d"), role);
 	if (role==ATSPI_ROLE_TERMINAL ||
 		(((role==ATSPI_ROLE_TEXT) || (role==ATSPI_ROLE_PASSWORD_TEXT)) &&
 		 state_set && atspi_state_set_contains(state_set, ATSPI_STATE_EDITABLE))) {
